d0e5a68a4637976474e568c3f8c7433bdcded8dc
Aurélien Bottazini <aurelien@bottazini.com>
2019-05-21 14:09:27 +0200

    scroll up

--- config.h
+++ config.h
@@ -192,6 +192,8 @@ static Shortcut shortcuts[] = {
         { MODKEY,               XK_o,           externalpipe,   {.v = openurlcmd } },
         { MODKEY,               XK_l,           externalpipe,   {.v = copyurlcmd } },
         { MODKEY,               XK_y,           externalpipe,   {.v = copyoutput } },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
